b9c1389a90a38a5382fb4a1b9dc8c835c08be7e4
Abena31 <129714347+Abena31@users.noreply.github.com>
2023-06-15 00:05:03 -0400

    updated index.html and java script

--- src/script.js
+++ src/script.js
@@ -57,20 +57,20 @@ function handleSubmit(event) {
   search(cityInputElement.value);
 }
 
-function displayfahrenheitTemperature(event) {
+function displayFahrenheitTemperature(event) {
   event.preventDefault();
   let temperatureElement = document.querySelector("#temperature");
 
   celsiusLink.classList.remove("active");
-  fahrenheitInk.classList.add("active");
-  let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
-  temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
+  fahrenheitLink.classList.add("active");
+  let fahrenheiTemperature = (celsiusTemperature * 9) / 5 + 32;
+  temperatureElement.innerHTML = Math.round(fahrenheiTemperature);
 }
 
 function displayCelsiusTemperature(event) {
   event.preventDefault();
   celsiusLink.classList.add("active");
-  fahrenheitInk.classList.remove("active");
+  fahrenheitLink.classList.remove("active");
   let temperatureElement = document.querySelector("#temperature");
   temperatureElement, (innerHTML = Math.round(celsiusTemperature));
 }
@@ -80,8 +80,8 @@ let celsiusTemperature = null;
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
 
-let fahrenheitInk = document.querySelector("#fahrenheit-ink");
-fahrenheitInk.addEventListener("click", displayfahrenheitTemperature);
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
 
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
